fe195c607e7cf01ba52b8fe08960b109901a0af2
smhrd <smhrd@59.0.234.207>
2023-07-20 18:03:40 +0900

    230720_18:03_최수환_Est_send.jsp 수정

---
+++ TravelMaker/src/main/java/controller/SendCon.java
@@ -0,0 +1,24 @@
+package controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import command.Command;
+
+@WebServlet("/SendCon")
+public class SendCon implements Command {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) {
+
+		
+		
+		
+		
+		
+		
+		return null;
+	}
+
+}
